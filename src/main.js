471a82c557c610e0d9b0fcccd13f21256d896699
kimhyunwoo <sleeper0512@naver.com>
2019-02-17 22:20:31 +0900

    백업

--- src/main.js
+++ src/main.js
@@ -7,3 +7,10 @@ new Vue({
 })
 
 import './assets/scss/catalog.scss';
+
+//메뉴 열기닫기
+var sidebarController = document.querySelector('.btn-fold');
+var sidebarPanel =  document.querySelector('.section-sidebar');
+sidebarController.addEventListener('click',function(){
+  sidebarPanel.classList.toggle("side__close");
+})
