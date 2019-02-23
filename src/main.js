2770bf60e7f181a45bfd1ce501ac78352d93a025
kimhyunwoo <sleeper0512@naver.com>
2019-02-24 00:07:57 +0900

    백업

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'expose-loader?$!expose-loader?jQuery!jquery'
 
 new Vue({
   el: '#app',
@@ -14,3 +15,19 @@ var sidebarPanel =  document.querySelector('.section-sidebar');
 sidebarController.addEventListener('click',function(){
   sidebarPanel.classList.toggle("side__close");
 })
+
+
+// GNB Bar 애니메이션
+$('.list-tab>li').on('mouseenter', function() {
+  var menuIndex = $(this).index();
+  var menuNum = $('.list-tab>li:eq('+menuIndex+') .link-tab');
+  var menuWid = parseInt( menuNum.outerWidth() );
+  var menuPos = parseInt( menuNum.position().left);
+  $('.bar_menu').stop().animate({
+    left : menuPos + parseInt((menuWid/2))
+  },200);
+  $('.inner_bar').stop().animate({
+    width : menuWid+3,
+    marginLeft : -(menuWid/2)
+  },400);
+});
